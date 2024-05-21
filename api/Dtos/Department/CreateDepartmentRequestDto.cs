c9b49bd58dbb4b8ef95349661be6728b91dc70ec
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:37:22 +0200

    department dto models

---
+++ api/Dtos/Department/CreateDepartmentRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Department;
+
+public class CreateDepartmentRequestDto
+{
+    [MinLength(2, ErrorMessage = "Department must be at least 2 characters")]
+    [MaxLength(15, ErrorMessage = "Department cannot be over 15 over characters")]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ api/Dtos/Department/DepartmentDto.cs
@@ -0,0 +1,12 @@
+﻿using api.Dtos.Employee;
+
+namespace api.Dtos.Department;
+
+public class DepartmentDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    public int? SupervisorId { get; set; }
+    public List<EmployeeDto> Employees { get; set; } = [];
+}
