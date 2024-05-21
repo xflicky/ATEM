a6873193d597f2aeab2e5280a1e1ce7b4c39dfa0
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:31:26 +0200

    attendance record base model

---
+++ api/Models/AttendanceRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models;
+
+public class AttendanceRecord
+{
+    public int Id { get; set; }
+    public DateTime EvidenceDate { get; set; }
+    
+    public string Type { get; set; } = string.Empty;
+    
+    public int? EmployeeId { get; set; }
+    public Employee? Employee { get; set; }
+}
