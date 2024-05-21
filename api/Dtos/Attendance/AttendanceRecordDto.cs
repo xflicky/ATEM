8b9cd711dacdc27bddf246a2cb563ca4cf3a84cc
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:36:25 +0200

    attendance dto models

---
+++ api/Dtos/Attendance/AttendanceRecordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Dtos.AttendanceRecord;
+
+public class AttendanceRecordDto
+{
+    public int Id { get; set; }
+    public int? EmployeeId { get; set; }
+    public DateTime EvidenceDate { get; set; }
+    public string Type { get; set; } = string.Empty;
+}
---
+++ api/Dtos/Attendance/AttendanceTimeSheetDto.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Dtos.AttendanceRecord;
+
+public class AttendanceTimeSheetDto
+{
+    public int? EmployeeId { get; set; }
+    public string EmployeeName { get; set; } = string.Empty;
+    public string EmployeeSurName { get; set; } = string.Empty;
+    
+    public int MonthPeriod { get; set; }
+    public int YearPeriod { get; set; }
+    
+    public List<AttendanceRecordDto> AttendanceRecords { get; set; } = [];
+}
