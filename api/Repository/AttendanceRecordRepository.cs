3a2d7100467d775e6ef87349f34d4266a070c19f
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:42:47 +0200

    interface and attendance record repository impl

---
+++ api/Repository/AttendanceRecordRepository.cs
@@ -0,0 +1,44 @@
+﻿using api.Data;
+using api.Mappers;
+using api.Models;
+using api.QueryObjects;
+using api.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository;
+
+public class AttendanceRecordRepository : IAttendanceRecordRepository
+{
+
+    private readonly ApplicationDbContext _dbContext;
+
+    public AttendanceRecordRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<AttendanceRecord> CreateAttendanceRecordAsync(AttendanceRecord attendanceRecord)
+    {
+        await _dbContext.AttendanceRecords.AddAsync(attendanceRecord);
+        await _dbContext.SaveChangesAsync();
+        return attendanceRecord;
+    }
+
+    public async Task<List<AttendanceRecord>> GetAllAttendanceRecordsByEmployeeAsync(Employee employee)
+    {
+        return await _dbContext.AttendanceRecords
+            .Where(ar => ar.EmployeeId == employee.Id)
+            .ToListAsync();
+    }
+
+    public async Task<List<AttendanceRecord>> GetAllAttendanceRecordsByQueryAsync(TimeSheetQueryParameters queryParams)
+    {
+        return await _dbContext.AttendanceRecords
+            .Where(ar => ar.EmployeeId == queryParams.EmployeeId
+                         && ar.EvidenceDate.Month >= queryParams.Month
+                         && ar.EvidenceDate.Year <= queryParams.Year)
+            .ToListAsync();
+    }
+
+}
---
+++ api/Repository/Interfaces/IAttendanceRecordRepository.cs
@@ -0,0 +1,11 @@
+﻿using api.Models;
+using api.QueryObjects;
+
+namespace api.Repository.Interfaces;
+
+public interface IAttendanceRecordRepository
+{
+    Task<AttendanceRecord> CreateAttendanceRecordAsync(AttendanceRecord attendanceRecord);
+    Task<List<AttendanceRecord>> GetAllAttendanceRecordsByQueryAsync(TimeSheetQueryParameters queryParams);
+    
+}
