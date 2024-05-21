e7df24d97e631f7102ea101fd683b143eac6eaf2
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:59:16 +0200

    age calculator utility

---
+++ api/Utils/AgeCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Utils;
+
+public static class AgeCalculator
+{
+    public static int CalculateAge(DateTime dateOfBirth)
+    {
+        var today = DateTime.Today;
+        var age = today.Year - dateOfBirth.Year;
+        if (dateOfBirth.Date > today.AddYears(-age)) age--;
+        return age;
+    }
+}
