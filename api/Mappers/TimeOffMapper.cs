079987e0fff01f1b9d6bfe6865d643381c2062fd
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:39:59 +0200

    timeoff mapper impl

---
+++ api/Mappers/TimeOffMapper.cs
@@ -0,0 +1,35 @@
+﻿using api.Dtos.TimeOff;
+using api.Models;
+
+namespace api.Mappers;
+
+public static class TimeOffMapper
+{
+    public static TimeOffDto ToTimeOffDto(TimeOff timeOffModel)
+    {
+        return new TimeOffDto
+        {
+            Id = timeOffModel.Id,
+            RequestDate = timeOffModel.RequestDate,
+            StartDate = timeOffModel.StartDate,
+            EndDate = timeOffModel.EndDate,
+            Reason = timeOffModel.Reason,
+            Approved = timeOffModel.Approved,
+            EmployeeId = timeOffModel.EmployeeId
+        };
+    }
+    
+    public static TimeOff ToTimeOffFromTimeOffRequestDto(CreateTimeOffRequestDto createTimeOffRequestDto, int employeeId)
+    {
+        return new TimeOff
+        {
+            RequestDate = DateTime.Now,
+            StartDate = DateOnly.ParseExact(createTimeOffRequestDto.StartDate, "dd.MM.yyyy", null),
+            EndDate = DateOnly.ParseExact(createTimeOffRequestDto.EndDate, "dd.MM.yyyy", null),
+            EmployeeId = employeeId,
+            Reason = createTimeOffRequestDto.Reason,
+            Approved = false
+        };
+    }
+
+}
