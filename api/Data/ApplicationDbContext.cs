4a3c077f7284263fd0d3ab8385ed35e652a6fbc2
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:50:05 +0200

    init db context

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+    public DbSet<TimeOff> TimeOffs { get; set; }
+    public DbSet<SickDay> SickDays { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>()
+            .HasMany(d => d.Employees)
+            .WithOne(e => e.Department) 
+            .HasForeignKey(e => e.DepartmentId);
+
+        modelBuilder.Entity<Department>()
+            .HasOne(d => d.Supervisor)
+            .WithMany()
+            .HasForeignKey(d => d.SupervisorId);
+
+        modelBuilder.Entity<AttendanceRecord>()
+            .HasOne(e => e.Employee)
+            .WithMany()
+            .HasForeignKey("EmployeeId");
+     
+        modelBuilder.Entity<TimeOff>()
+            .HasOne(e => e.Employee)
+            .WithMany()
+            .HasForeignKey("EmployeeId");
+
+        modelBuilder.Entity<SickDay>()
+            .HasOne(e => e.Employee)
+            .WithMany()
+            .HasForeignKey("EmployeeId");
+    }
+}
