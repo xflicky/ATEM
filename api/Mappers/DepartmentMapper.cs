bf633bfcab5f1f5ca5e82c0e059d5dc7fbe503a2
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:40:44 +0200

    department mapper impl

---
+++ api/Mappers/DepartmentMapper.cs
@@ -0,0 +1,31 @@
+﻿using api.Dtos.Department;
+using api.Models;
+
+namespace api.Mappers;
+
+public static class DepartmentMapper
+{
+    public static DepartmentDto ToDepartmentDto(Department departmentModel)
+    {
+        return new DepartmentDto
+        {
+            Id = departmentModel.Id,
+            Name = departmentModel.Name,
+            SupervisorId = departmentModel.SupervisorId,
+            
+            Employees = departmentModel.Employees
+                .Where(employee => !employee.IsDeleted)
+                .Select(employee => EmployeeMapper.ToEmployeeDto(employee))
+                .ToList()
+        };
+    }
+    
+    public static Department ToDepartmentFromCreateDto(CreateDepartmentRequestDto departmentDto)
+    {
+        return new Department
+        {
+            RegistrationDate = DateTime.Now,
+            Name = departmentDto.Name
+        };
+    }
+}
