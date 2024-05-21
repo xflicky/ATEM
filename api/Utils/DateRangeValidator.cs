c47a1757de17ea2f2a0976a9c79ed226afae7219
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:59:34 +0200

    date range validator utility

---
+++ api/Utils/DateRangeValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Utils;
+
+public static class DateRangeValidator
+{
+    public static bool IsDateWithinRange(DateOnly startDate, DateOnly endDate, int minDays, int maxDays)
+    {
+        DateTime startDateTime = startDate.ToDateTime(TimeOnly.MinValue);
+        DateTime endDateTime = endDate.ToDateTime(TimeOnly.MinValue);
+        
+        TimeSpan dateSpan = endDateTime - startDateTime;
+        
+        return dateSpan.Days >= minDays && dateSpan.Days <= maxDays;
+    }
+}
