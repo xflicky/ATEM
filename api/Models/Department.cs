7bf16e687e707d1b219a8a27e667b74ecf856b90
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:31:38 +0200

    department base model

---
+++ api/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace api.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    
+    public int? SupervisorId { get; set; }
+    public Employee? Supervisor { get; set; }
+    public List<Employee> Employees { get; set; } = [];
+    
+    public DateTime RegistrationDate { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
