98d2480c0e1406a93961cc62ca6fe2bc8b296921
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:36:38 +0200

    timeoff dto models

---
+++ api/Dtos/TimeOff/CreateTimeOffRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using api.Attributes;
+using api.Attributes.Date;
+using api.Constants;
+
+namespace api.Dtos.TimeOff;
+
+public class CreateTimeOffRequestDto
+{
+    [Required(ErrorMessage = "Start date is required.")]
+    [ValidDateFormat(DateConstants.DateFormat)]
+    [MinCurrentDateFormat]
+    public string StartDate { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "End date is required.")]
+    [ValidDateFormat(DateConstants.DateFormat)]
+    [MinCurrentDateFormat]
+    public string EndDate { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Reason is required.")]
+    [MinLength(10, ErrorMessage = "Reason must be at least 10 characters")]
+    [MaxLength(200, ErrorMessage = "Reason cannot be over 200 characters")]
+    public string Reason { get; set; } = string.Empty;
+}
---
+++ api/Dtos/TimeOff/TimeOffDto.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Dtos.TimeOff;
+
+public class TimeOffDto
+{
+    public int Id { get; set; }
+    public int? EmployeeId { get; set; }
+    
+    public DateTime RequestDate { get; set; }
+    
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public string Reason { get; set; } = string.Empty;
+    public bool Approved { get; set; }
+}
