2cc0ad043e9974f78ff7eca43b5b21da54d5ed69
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:32:11 +0200

    sickday base model

---
+++ api/Models/SickDay.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Models;
+
+public class SickDay
+{
+    public int Id { get; set; }
+    public DateTime RequestDate { get; set; }
+
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public string Message { get; set; } = string.Empty;
+
+    public int? EmployeeId { get; set; }
+    public Employee? Employee { get; set; }
+}
