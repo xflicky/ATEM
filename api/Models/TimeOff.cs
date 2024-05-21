e8db89109f3c105feb3d5bd46bca8791ebc281a0
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:33:45 +0200

    timeoff base model

---
+++ api/Models/TimeOff.cs
@@ -0,0 +1,16 @@
+﻿namespace api.Models;
+
+public class TimeOff
+{
+    public int Id { get; set; }
+    public DateTime RequestDate { get; set; }
+
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+
+    public string Reason { get; set; } = string.Empty;
+    public bool Approved { get; set; }
+
+    public int? EmployeeId { get; set; }
+    public Employee? Employee { get; set; }
+}
