2cc877b4600b96e39633e18c5a17b1f8e6f2881d
Radoslav Banik <rabamisk@gmail.com>
2024-05-21 10:37:04 +0200

    sickday dto models

---
+++ api/Dtos/SickDay/CreateSickDayAnnouncementDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using api.Attributes;
+using api.Attributes.Date;
+using api.Constants;
+
+namespace api.Dtos.SickDay;
+
+public class CreateSickDayAnnouncementDto
+{
+    [Required(ErrorMessage = "Start date is required.")]
+    [MinCurrentDateFormat]
+    public string StartDate { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "End date is required.")]
+    [MinCurrentDateFormat]
+    public string EndDate { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Message is required.")]
+    [MinLength(10, ErrorMessage = "Message must be at least 10 characters")]
+    [MaxLength(200, ErrorMessage = "Message cannot be over 100 characters")]
+    public string Message { get; set; } = string.Empty;
+}
---
+++ api/Dtos/SickDay/SickDayDto.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Dtos.SickDay;
+
+public class SickDayDto
+{
+    public int? EmployeeId { get; set; }
+    
+    public DateTime RequestDate { get; set; }
+
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    
+    public string Message { get; set; } = string.Empty;
+}
